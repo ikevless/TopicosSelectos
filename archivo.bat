9cc936fb8721e1c5dfbb12bf7cf9475bf6f2136b
kevin <ikevinhuerta@gmail.com>
2021-10-29 23:12:17 -0700

    añadido archvio ejecutable de estructura

---
+++ archivo.bat
@@ -0,0 +1,32 @@
+@echo off
+echo.
+echo 
+echo TopicosSelectos/
+echo 	Controlador
+echo 		Login/
+echo 			Login.cs
+echo  		Menu/
+echo  			Menu.cs	
+echo  		Registro/
+echo 			Registro.cs
+echo 	Modelos/
+echo 		Administrador/
+echo 			Administrador.txt
+echo 		Animal/
+echo 			Animal.txt
+echo 		Operador/
+echo 			Operador.txt
+echo 	vistas/
+echo 		Login/
+echo 			Login.cs
+echo 			Login.designer.cs
+echo 		Menu/
+echo 			Menu.cs	
+echo 			Menu.designer.cs
+echo 		Registro/
+echo 			Registro.cs
+echo 			Registro.designer.cs
+
+
+echo.
+pause
